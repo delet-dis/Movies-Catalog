0162b315bd5216de3dcd6c55754e6ad50e4f4ada
delet-dis <efimov.036@gmail.com>
2022-10-15 11:14:18 +0700

    🙋🏼 authentication business logic done

--- app/src/main/java/com/delet_dis/moviescatalog/domain/loginView/LoginState.kt
+++ app/src/main/java/com/delet_dis/moviescatalog/domain/loginView/LoginState.kt
@@ -1,8 +1,9 @@
 package com.delet_dis.moviescatalog.domain.loginView
 
 data class LoginState(
-    val isLoginIncorrect: Boolean = false,
+    val isUsernameIncorrect: Boolean = false,
     val isPasswordIncorrect: Boolean = false,
+    val isLoginButtonActive: Boolean = false,
     val errorText: String? = null,
     val isLoading: Boolean = false
 )
